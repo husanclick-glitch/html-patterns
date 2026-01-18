3a6be1d447f5f1f7d215438229586f9ef8713ef5
Хусан А. Неъматуллаев <nematullayev.h@corp.click.uz>
2026-01-18 14:54:48 +0500

    refactor: change toggle fucntion for accordion

--- patterns/accordion/script.js
+++ patterns/accordion/script.js
@@ -2,47 +2,90 @@
 'use strict';
 
 class Accordion {
-  constructor(domNode) {
-    this.rootEl = domNode;
-    this.buttonEl = this.rootEl.querySelector('button[aria-expanded]');
-
-    const controlsId = this.buttonEl.getAttribute('aria-controls');
-    this.contentEl = document.getElementById(controlsId);
-
-    this.open = this.buttonEl.getAttribute('aria-expanded') === 'true';
-
-    this.buttonEl.addEventListener('click', this.onButtonClick.bind(this));
+  constructor() {
+    this.rootElements = document.querySelectorAll("[data-accordion]");
   }
 
-  onButtonClick() {
-    this.toggle(!this.open);
+  init() {
+    this.rootElements.forEach((element) => (
+      element.addEventListener("click", this.onAccordionClick.bind(this))
+    ))
   }
 
-  toggle(open) {
-    if (open === this.open) {
-      return;
+  onAccordionClick(event) {
+    var target = event.target;
+    var parent = target.closest("[data-accordion]");
+    var control = target.closest("[aria-controls]");
+
+    if (parent && control) {
+      var contentId = control.getAttribute("aria-controls");
+
+      var content = parent.querySelector(
+        `#${contentId}`
+      );
+
+      this.toggle(target, parent, control, content);
     }
+  }
 
-    this.open = open;
+  toggle(target, parent, control, content) {
+    var open = control.getAttribute("aria-expanded") === "true";
 
-    this.buttonEl.setAttribute('aria-expanded', `${open}`);
-    if (open) {
-      this.contentEl.removeAttribute('hidden');
+    var toggler = !open;
+
+    control.setAttribute("aria-expanded", `${toggler}`);
+    if (toggler) {
+      content.removeAttribute("hidden");
     } else {
-      this.contentEl.setAttribute('hidden', '');
+      content.setAttribute("hidden", "");
     }
-  }
-
-  open() {
-    this.toggle(true);
-  }
-
-  close() {
-    this.toggle(false);
   }
 }
 
-var accordions = document.querySelectorAll('.accordion h3');
-accordions.forEach((accordionEl) => {
-  new Accordion(accordionEl);
-});
+new Accordion().init();
+
+// class Accordion {
+//   constructor(domNode) {
+//     this.rootEl = domNode;
+//     this.buttonEl = this.rootEl.querySelector('button[aria-expanded]');
+
+//     const controlsId = this.buttonEl.getAttribute('aria-controls');
+//     this.contentEl = document.getElementById(controlsId);
+
+//     this.open = this.buttonEl.getAttribute('aria-expanded') === 'true';
+
+//     this.buttonEl.addEventListener('click', this.onButtonClick.bind(this));
+//   }
+
+//   onButtonClick() {
+//     this.toggle(!this.open);
+//   }
+
+//   toggle(open) {
+//     if (open === this.open) {
+//       return;
+//     }
+
+//     this.open = open;
+
+//     this.buttonEl.setAttribute('aria-expanded', `${open}`);
+//     if (open) {
+//       this.contentEl.removeAttribute('hidden');
+//     } else {
+//       this.contentEl.setAttribute('hidden', '');
+//     }
+//   }
+
+//   open() {
+//     this.toggle(true);
+//   }
+
+//   close() {
+//     this.toggle(false);
+//   }
+// }
+
+// const accordions = document.querySelectorAll('.accordion h3');
+// accordions.forEach((accordionEl) => {
+//   new Accordion(accordionEl);
+// });
